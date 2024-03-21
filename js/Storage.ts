408113c6d57662444ea5be881fc6d0ea12ea2e2a
Mateusz <m.zaniewski1995@gmail.com>
2024-03-21 18:31:16 +0100

    add parcel and timer new classes

--- js/Storage.ts
+++ js/Storage.ts
@@ -1,3 +1,20 @@
 export class StorageData {
+  private pomodoroTime: number;
+  private shortBreakTime: number;
+  private LongBreakTime: number;
+  private autoBreakStart: boolean;
+  private autoPomoStart: boolean;
+  private longBreakInterval: number;
+  private alarmSound: string;
+  private alarmvolume: number;
+  private tickingSound: string;
+  private tickingVolume: number;
   constructor() {}
+
+  get getPomodoroTime() {
+    return this.pomodoroTime;
+  }
+  set setPomodoroTime(PomodoroTime: number) {
+    this.pomodoroTime = PomodoroTime;
+  }
 }
--- js/timer.ts
+++ js/timer.ts
@@ -1,3 +1,114 @@
-export class Timer {
-  constructor() {}
+interface TimerInterface {
+  execute(): void;
+  startTimer(value: number): void;
+  stopTimer(): void;
+  changeColors(colorClass: string): void;
 }
+
+export class PomodoroTimerClass implements TimerInterface {
+  constructor(protected pomoBtn: HTMLButtonElement | null) {}
+
+  execute() {
+    this.startTimer(25);
+    console.log("wlazłem do pomodoro");
+  }
+  startTimer(value: number) {
+    console.log(value);
+  }
+  stopTimer() {
+    console.log("stop");
+  }
+  changeColors(colorClass: string) {}
+}
+
+export class ShortTimerClass implements TimerInterface {
+  constructor(protected pomoBtn: HTMLButtonElement | null) {}
+
+  execute() {
+    this.startTimer(5);
+    console.log("wlazłem do short break");
+  }
+  startTimer(value: number) {
+    console.log(value);
+  }
+  stopTimer() {
+    console.log("stop");
+  }
+  changeColors(colorClass: string) {
+    console.log("zmiana klasy" + colorClass);
+  }
+}
+
+export class LongTimerClass implements TimerInterface {
+  constructor(protected pomoBtn: HTMLButtonElement | null) {}
+  execute() {
+    this.startTimer(15);
+    console.log("wlazłem do long break");
+  }
+  startTimer(value: number) {
+    console.log(value);
+  }
+  stopTimer() {
+    console.log("stop");
+  }
+  changeColors(colorClass: string) {
+    console.log("zmiana klasy" + colorClass);
+  }
+}
+class TimerHandler {
+  constructor(
+    private timer: TimerInterface,
+    protected background: HTMLBodyElement | null,
+    protected startButton: HTMLButtonElement | null,
+    protected clockMinutes: HTMLSpanElement | null,
+    protected clockSeconds: HTMLSpanElement | null
+  ) {}
+  start() {
+    this.timer.execute();
+  }
+}
+
+const Pomodoro = new PomodoroTimerClass(document.querySelector(".clock__one"));
+const ShortTimer = new ShortTimerClass(document.querySelector(".clock__two"));
+const LongTimer = new LongTimerClass(document.querySelector(".clock__three"));
+const background = document.body as HTMLBodyElement;
+const startButton = document.querySelector(
+  ".clock__start"
+) as HTMLButtonElement;
+const clockMinutes = document.querySelector(
+  ".clock__minutes"
+) as HTMLSpanElement;
+const clockSeconds = document.querySelector(
+  ".clock__seconds"
+) as HTMLSpanElement;
+
+document.querySelector(".clock__one")?.addEventListener("click", () => {
+  const timer = new TimerHandler(
+    Pomodoro,
+    background,
+    startButton,
+    clockMinutes,
+    clockSeconds
+  );
+  timer.start();
+});
+document.querySelector(".clock__two")?.addEventListener("click", () => {
+  const timer = new TimerHandler(
+    ShortTimer,
+    background,
+    startButton,
+    clockMinutes,
+    clockSeconds
+  );
+  timer.start();
+});
+document.querySelector(".clock__three")?.addEventListener("click", () => {
+  const timer = new TimerHandler(
+    LongTimer,
+    background,
+    startButton,
+    clockMinutes,
+    clockSeconds
+  );
+  timer.start();
+});
