43c3e1889315b7bd570c838481490816e6e7d703
Mateusz <m.zaniewski1995@gmail.com>
2024-04-13 12:45:48 +0200

    minor change

--- js/Main.ts
+++ js/Main.ts
@@ -1,8 +1,8 @@
 class MainClass {
-  private pomodoroTime: number = 25;
-  private shortBreakTime: number = 5;
-  private longBreakTime: number = 15;
-  private seconds: number = 59;
+  private pomodoroTime: number;
+  private shortBreakTime: number;
+  private longBreakTime: number;
+  private seconds: number;
   private intervalSeconds;
   private intervalMinutes;
   private startPomodoroAutomatically;
@@ -11,26 +11,35 @@ class MainClass {
   constructor(
     private background: HTMLBodyElement,
     private startButton: HTMLButtonElement,
+    private resetButton: HTMLButtonElement,
     private clockMinutes: HTMLSpanElement,
     private clockSeconds: HTMLSpanElement,
     private pomodoroButton: HTMLSpanElement,
     private shortButton: HTMLSpanElement,
     private longButton: HTMLSpanElement
   ) {}
-  execute() {
+  start() {
     this.startTimer(this.pomodoroTime, this.seconds);
-    // this.changeColors();
+    this.changeButtonColor("orange");
+    this.changeBackgroundColor("orange");
+    this.showResetButton();
+    this.buttonTextContent("Stop");
+    this.buttonStartDataset("true");
+  }
+  stop() {
+    this.stopTimer();
+    this.hideResetButton();
+    this.buttonTextContent("Start");
+    this.buttonStartDataset("false");
   }
   startTimer(minutes: number, seconds: number) {
-    this.changeBackgroundColor("orange");
-    this.startButton.textContent = "Stop";
     minutes -= 1;
     this.clockMinutes.textContent = minutes < 10 ? `0${minutes}` : String(minutes);
     this.clockSeconds.textContent = String(seconds);
 
     this.intervalSeconds = setInterval(() => {
-      this.clockSeconds.textContent = seconds < 10 ? `0${seconds}` : String(seconds);
       seconds -= 1;
+      this.clockSeconds.textContent = seconds < 10 ? `0${seconds}` : String(seconds);
       if (seconds == 0) {
         seconds = 60;
       }
@@ -39,7 +48,18 @@ class MainClass {
       minutes -= 1;
       this.clockMinutes.textContent = minutes < 10 ? `0${minutes}` : String(minutes);
     }, 60000);
-    this.startButton.dataset.start = "true";
+  }
+  buttonTextContent(text: string) {
+    this.startButton.textContent = text;
+  }
+  buttonStartDataset(flag: string) {
+    this.startButton.dataset.start = flag;
+  }
+  showResetButton() {
+    this.resetButton.classList.add("reset--visible");
+  }
+  hideResetButton() {
+    this.resetButton.classList.remove("reset--visible");
   }
   changeBackgroundColor(color: string) {
     this.background.classList.remove("orange");
@@ -47,19 +67,24 @@ class MainClass {
     this.background.classList.remove("green");
     this.background.classList.add(color);
   }
+  changeButtonColor(color: string) {
+    this.startButton.classList.remove("orange");
+    this.startButton.classList.remove("blue");
+    this.startButton.classList.remove("green");
+    this.startButton.classList.add(color);
+  }
   stopTimer() {
-    this.startButton.textContent = "Start";
     clearInterval(this.intervalMinutes);
     clearInterval(this.intervalSeconds);
-    this.startButton.dataset.start = "false";
   }
-  changeColors(colorClass: string) {
-    this.pomodoroButton.classList.add("clock__button--active");
+  changeTimerButtonActive(button) {
+    button.classList.add("clock__button--active");
   }
 }
 const main = new MainClass(
   document.body as HTMLBodyElement,
   document.querySelector(".clock__start") as HTMLButtonElement,
+  document.querySelector(".reset") as HTMLButtonElement,
   document.querySelector(".clock__minutes") as HTMLSpanElement,
   document.querySelector(".clock__seconds") as HTMLSpanElement,
   document.querySelector(".clock__one") as HTMLSpanElement,
@@ -68,9 +93,8 @@ const main = new MainClass(
 );
 document.querySelector(".clock__start")?.addEventListener("click", function () {
   if (this.dataset.start == "false") {
-    main.execute();
+    main.start();
   } else {
-    main.stopTimer();
+    main.stop();
   }
 });
-// document.querySelector(".clock__start")?.addEventListener("click", () => {});
